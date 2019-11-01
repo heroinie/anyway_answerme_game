8ac238985b7b377921656e9c98e0b83b1a7868ff
heroinie <56429944+heroinie@users.noreply.github.com>
2019-11-01 21:00:06 +0900

    Add files via upload

---
+++ 답정너게임.py
@@ -0,0 +1,17 @@
+import random
+
+questions = [
+	"너는 무슨 과일을 가장 좋아하니?"
+	"과일은 뭐니뭐니해도 빨갛고 둥글어야지. 좋아하는 과일이 뭐니?"
+	"아니, 사과 몰라? 제일 좋아하는 과일이 뭐냐니까,,"
+]
+
+answer = input(questions[0])
+
+while true:
+	if answer == '사과'
+		break
+
+	answer = input(random.choice(questions))
+
+print("맞아, 사과가 제일 맛이쪄.")
